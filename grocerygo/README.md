032438b1f6490ba79e7a29c4703d0061b1f5e332
shivanshlearnify <kesarwani.shivansh@gmail.com>
2024-08-25 00:02:21 +0530

    created next app

---
+++ grocerygo/README.md
@@ -0,0 +1,3 @@
+
+1 -> Creating next app
+2 -> 
---
+++ grocerygo/app/productCategory/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const productCategory = () => {
+  return (
+    <div>product-category</div>
+  )
+}
+
+export default productCategory
